3cafd10b9d15e188faefb8c3355a2ec43c23ec80
Elchin Ismailov <ismailowwork@gmail.com>
2025-11-06 09:59:38 +0400

    test: test UITextFields are initially empty and have a referencing outlet

--- PhotoApp/Signup/Controllers/SignupViewController.swift
+++ PhotoApp/Signup/Controllers/SignupViewController.swift
@@ -15,7 +15,12 @@ class SignupViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatPasswordTextField: UITextField!
+    
     /*
     // MARK: - Navigation
 
--- PhotoAppTests/Signup/Controllers/SignupViewControllerTests.swift
+++ PhotoAppTests/Signup/Controllers/SignupViewControllerTests.swift
@@ -9,18 +9,36 @@ import XCTest
 @testable import PhotoApp
 
 final class SignupViewControllerTests: XCTestCase {
-
+    
+    var storyboard: UIStoryboard!
+    var sut: SignupViewController!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        storyboard = UIStoryboard(name: "Main", bundle: nil)
+        sut = storyboard.instantiateViewController(identifier: "SignupViewController") as? SignupViewController
+        sut?.loadViewIfNeeded()
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        storyboard = nil
+        sut = nil
     }
-
-    func testSignupViewController_WhenCreated_HasRequiredTextFieldsEmpty() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let sut = storyboard.instantiateViewController(identifier: "SignupViewController") as? SignupViewController
+    
+    func testSignupViewController_WhenCreated_HasRequiredTextFieldsEmpty() throws {
+        
+        let firstNameTextField = try XCTUnwrap(sut.firstNameTextField, "The firstNameTextField is not connected to an IBOutlet")
+        let lastNameTextField = try XCTUnwrap(sut.lastNameTextField, "The lastNameTextField is not connected to an IBOutlet")
+        let emailTextField = try XCTUnwrap(sut.emailTextField, "The emailTextField is not connected to an IBOutlet")
+        let passwordTextField = try XCTUnwrap(sut.emailTextField, "The passwordTextField is not connected to an IBOutlet")
+        let repeatPasswordTextField = try XCTUnwrap(sut.repeatPasswordTextField, "The repeatPasswordTextField is not connected to an IBOutlet")
+        
+        XCTAssertEqual(firstNameTextField.text, "", "First name text field was not empty when the view controller initially loaded")
+        XCTAssertEqual(lastNameTextField.text, "", "Last name text field was not empty when the view controller initially loaded")
+        XCTAssertEqual(emailTextField.text, "", "Email text field was not empty when the view controller initially loaded")
+        XCTAssertEqual(passwordTextField.text, "", "Password text field was not empty when the view controller initially loaded")
+        XCTAssertEqual(repeatPasswordTextField.text, "", "Repeat Password text field was not empty when the view controller initially loaded")
     }
-
+    
 }
