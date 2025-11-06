d3f5b52a9fbd37dbe02e55819146ec8d1a99afc9
Elchin Ismailov <ismailowwork@gmail.com>
2025-11-06 11:31:12 +0400

    test: test UIButton has IBAction assigned to it

--- PhotoApp/Signup/Controllers/SignupViewController.swift
+++ PhotoApp/Signup/Controllers/SignupViewController.swift
@@ -20,15 +20,10 @@ class SignupViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var repeatPasswordTextField: UITextField!
+    @IBOutlet weak var signupButton: UIButton!
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func signupButtonTapped(_ sender: Any) {
     }
-    */
-
+    
 }
--- PhotoAppTests/Signup/Controllers/SignupViewControllerTests.swift
+++ PhotoAppTests/Signup/Controllers/SignupViewControllerTests.swift
@@ -41,4 +41,12 @@ final class SignupViewControllerTests: XCTestCase {
         XCTAssertEqual(repeatPasswordTextField.text, "", "Repeat Password text field was not empty when the view controller initially loaded")
     }
     
+    func testSignupViewController_WhenCreated_HasSignupButtonAndAction() throws {
+        let signupButton: UIButton = try XCTUnwrap(sut.signupButton, "Signup button does not have a referencing outlet")
+        let signupButtonActions = try XCTUnwrap(signupButton.actions(forTarget: sut, forControlEvent: .touchUpInside), "Signup button does not have any actions assigned to it")
+        
+        XCTAssertEqual(signupButtonActions.count, 1)
+        XCTAssertEqual(signupButtonActions.first, "signupButtonTapped:", "There is no action with a name signupButtonTapped assigned to signup button")
+    }
+    
 }
